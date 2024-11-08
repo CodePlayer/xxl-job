01a70b3db1bbd270d9f0f7e4463012c7946bf166
CodePlayer <codeplayer360@gmail.com>
2024-11-08 16:49:45 +0800

    改进：使用 ThreadPoolTaskExecutor 重构线程池

--- xxl-job-admin/src/main/java/com/xxl/job/admin/core/thread/JobTriggerPoolHelper.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/core/thread/JobTriggerPoolHelper.java
@@ -8,6 +8,7 @@ import com.xxl.job.admin.core.trigger.TriggerTypeEnum;
 import com.xxl.job.admin.core.trigger.XxlJobTrigger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 /**
  * job trigger thread pool helper
@@ -16,55 +17,48 @@ import org.slf4j.LoggerFactory;
  */
 public class JobTriggerPoolHelper {
 
-	private static Logger logger = LoggerFactory.getLogger(JobTriggerPoolHelper.class);
+	private static final Logger logger = LoggerFactory.getLogger(JobTriggerPoolHelper.class);
 
 	// ---------------------- trigger pool ----------------------
-
 	// fast/slow thread pool
-	private ThreadPoolExecutor fastTriggerPool = null;
-	private ThreadPoolExecutor slowTriggerPool = null;
+	ThreadPoolTaskExecutor fastTriggerPool;
+	ThreadPoolTaskExecutor slowTriggerPool;
+	// job timeout count
+	volatile long minTim = System.currentTimeMillis() / 60000; // ms -> min
+	final ConcurrentMap<Integer, AtomicInteger> jobTimeoutCountMap = new ConcurrentHashMap<>();
 
 	public void start() {
-		fastTriggerPool = new ThreadPoolExecutor(
-				10,
-				XxlJobAdminConfig.getAdminConfig().getTriggerPoolFastMax(),
-				60L,
-				TimeUnit.SECONDS,
-				new LinkedBlockingQueue<>(1000),
-				r -> new Thread(r, "xxl-job, admin JobTriggerPoolHelper-fastTriggerPool-" + r.hashCode()));
+		XxlJobAdminConfig config = XxlJobAdminConfig.getAdminConfig();
 
-		slowTriggerPool = new ThreadPoolExecutor(
-				10,
-				XxlJobAdminConfig.getAdminConfig().getTriggerPoolSlowMax(),
-				60L,
-				TimeUnit.SECONDS,
-				new LinkedBlockingQueue<>(2000),
-				r -> new Thread(r, "xxl-job, admin JobTriggerPoolHelper-slowTriggerPool-" + r.hashCode()));
+		fastTriggerPool = createExecutor(config.getTriggerPoolFastMax(), "xxl-job-fastTriggerPool-");
+		slowTriggerPool = createExecutor(config.getTriggerPoolSlowMax(), "xxl-job-slowTriggerPool-");
+	}
+
+	static ThreadPoolTaskExecutor createExecutor(int max, String namePrefix) {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(10);
+		executor.setMaxPoolSize(max);
+		executor.setKeepAliveSeconds(60);
+		executor.setQueueCapacity(2000);
+		executor.setThreadNamePrefix(namePrefix);
+		executor.initialize();
+		return executor;
 	}
 
 	public void stop() {
-		//triggerPool.shutdown();
-		fastTriggerPool.shutdownNow();
-		slowTriggerPool.shutdownNow();
+		// triggerPool.shutdown();
+		fastTriggerPool.shutdown();
+		slowTriggerPool.shutdown();
 		logger.info(">>>>>>>>> xxl-job trigger thread pool shutdown success.");
 	}
-
-	// job timeout count
-	private volatile long minTim = System.currentTimeMillis() / 60000;     // ms > min
-	private volatile ConcurrentMap<Integer, AtomicInteger> jobTimeoutCountMap = new ConcurrentHashMap<>();
 
 	/**
 	 * add trigger
 	 */
-	public void addTrigger(final int jobId,
-	                       final TriggerTypeEnum triggerType,
-	                       final int failRetryCount,
-	                       final String executorShardingParam,
-	                       final String executorParam,
-	                       final String addressList) {
+	public void addTrigger(final int jobId, final TriggerTypeEnum triggerType, final int failRetryCount, final String executorShardingParam, final String executorParam, final String addressList) {
 
 		// choose thread pool
-		ThreadPoolExecutor triggerPool_ = fastTriggerPool;
+		ThreadPoolTaskExecutor triggerPool_ = fastTriggerPool;
 		AtomicInteger jobTimeoutCount = jobTimeoutCountMap.get(jobId);
 		if (jobTimeoutCount != null && jobTimeoutCount.get() > 10) {      // job-timeout 10 times in 1 min
 			triggerPool_ = slowTriggerPool;
@@ -105,7 +99,7 @@ public class JobTriggerPoolHelper {
 
 	// ---------------------- helper ----------------------
 
-	private static JobTriggerPoolHelper helper = new JobTriggerPoolHelper();
+	static final JobTriggerPoolHelper helper = new JobTriggerPoolHelper();
 
 	public static void toStart() {
 		helper.start();
